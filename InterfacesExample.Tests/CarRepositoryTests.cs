5f6a78156b60780ac08df513dce89f996f1f2971
Barbora Slavíková <barbora.slavikova@student.spsmb.cz>
2024-11-27 15:05:18 +0100

    Initial commit

---
+++ InterfacesExample.Tests/CarRepositoryTests.cs
@@ -0,0 +1,100 @@
+﻿namespace InterfacesExample.Tests;
+
+public class CarRepositoryTests
+{
+    [Fact]
+    public void InsertingNewModel_ShouldIncreaseRecordCount()
+    {
+        //Arrange
+        IRespository<CarModel> model = new CarRepository();
+        CarModel carModel = new CarModel("superb", "skoda");
+        int RecordCountBefore = model.RecordCount();
+
+        //Act
+        model.Insert(carModel);
+        int RecordCountAfter = model.RecordCount();
+
+        //Assert
+        Assert.Equal(RecordCountBefore, RecordCountAfter - 1);
+    }
+
+    [Fact]
+    public void InsertingNull_ShouldSustainRecordCount()
+    {
+        //Arrange
+        IRespository<CarModel> model = new CarRepository();
+        CarModel carModel = null;
+        int RecordCountBefore = model.RecordCount();
+
+        //Act
+        if (carModel != null)
+        {
+            model.Insert(carModel);
+        }
+
+        int RecordCountAfter = model.RecordCount();
+
+        //Assert
+        Assert.Equal(RecordCountBefore, RecordCountAfter);
+    }
+
+    [Fact]
+    public void GettingAllRecords_WithTwoRecords_ShouldReturnListOfTwoRecords()
+    {
+        //Arrange
+        IRespository<CarModel> model = new CarRepository();
+        CarModel carModel = new CarModel("superb", "skoda");
+        CarModel carModel2 = new CarModel("nesuperb", "neskoda");
+
+
+        //Act
+        model.Insert(carModel);
+        model.Insert(carModel2);
+        List<CarModel> models = model.Get();
+
+        //Assert
+        Assert.Equal(2, models.Count);
+        Assert.Contains(carModel, models);
+        Assert.Contains(carModel2, models);
+    }
+
+    [Fact]
+    public void GettingInsertedRecordWithId_WithTwoRecords_ShouldReturnInsertedRecord()
+    {
+        //Arrange
+        IRespository<CarModel> model = new CarRepository();
+        CarModel carModel = new CarModel("superb", "skoda");
+        CarModel carModel2 = new CarModel("nesuperb", "neskoda");
+
+
+        //Act
+        model.Insert(carModel);
+        model.Insert(carModel2);
+
+        CarModel getModel = model.Get(carModel.Id);
+        CarModel getModel2 = model.Get(carModel2.Id);
+
+        //Assert
+        Assert.Equal(carModel.Id, getModel.Id);
+        Assert.Equal(carModel2.Id, getModel2.Id);
+    }
+
+    [Fact]
+    public void GettingNotInsertedRecordWithId_WithTwoRecords_ShouldReturnNull()
+    {
+        //Arrange
+        IRespository<CarModel> model = new CarRepository();
+        CarModel carModel = new CarModel("superb", "skoda");
+        CarModel carModel2 = new CarModel("nesuperb", "neskoda");
+
+        //Act
+        model.Insert(carModel);
+        model.Insert(carModel2);
+        Guid noId = new Guid();
+        CarModel noCarModel = model.Get(noId);
+
+        //Assert
+        Assert.Null(noCarModel);
+    }  
+}
+
---
+++ InterfacesExample/CarRepository.cs
@@ -0,0 +1,55 @@
+﻿namespace InterfacesExample;
+
+public class CarRepository : IRespository<CarModel>
+{
+    public List<CarModel> CarList = new(); 
+    public CarModel? Get(Guid Id)
+    {
+        foreach (var carModel in CarList)
+        {
+            if (carModel.Id == Id)
+            {
+                return carModel;
+            }
+        }
+
+        return null;
+    }
+
+    public List<CarModel> Get()
+    {
+        return CarList;
+    }
+
+    public void Insert(CarModel model)
+    {
+        if (model != null)
+        {
+            CarList.Add(model);
+        }
+    }
+
+    public void Update(CarModel model)
+    {
+        CarModel existingModel = Get(model.Id);
+        if (existingModel != null)
+        {
+            existingModel.Name = model.Name;
+            existingModel.Brand = model.Brand;
+        }
+    }
+
+    public void Delete(Guid Id)
+    {
+        CarModel DeleteCar = Get(Id);
+        if (DeleteCar != null)
+        {
+            CarList.Remove(DeleteCar);
+        }
+    }
+
+    public int RecordCount()
+    {
+        return CarList.Count;
+    }
+}
